bbada36de861187ab046970d073340124fc7a849
karlyabut <karleisenyabut@gmail.com>
2019-09-16 21:01:47 +0000

    Implemented INNER JOINS

---
+++ 2_queries_joins/1_student_total_duration.sql
@@ -0,0 +1,3 @@
+SELECT sum(assignment_submissions.duration) AS total_duration
+FROM assignment_submissions JOIN students ON students.id = assignment_submissions.student_id
+WHERE students.name = 'Ibrahim Schimmel';
